dd121e6a25e398ae6d3a30dffbd3abccc650b9b4
ddqre12345 <629207639@qq.com>
2017-05-08 01:05:38 +0800

    添加介绍

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,9 @@
       player,
       loading
     },
+    mounted () {
+      console.log('%c 浅滩戏虾","background-image:-webkit-gradient( linear, left top,right top, color-stop(0, #00a419),color-stop(0.15, #f44336), color-stop(0.29, #ff4300),color-stop(0.3, #AA00FF),color-stop(0.4, #8BC34A), color-stop(0.45, #607D8B),color-stop(0.6, #4096EE), color-stop(0.75, #D50000),color-stop(0.9, #4096EE), color-stop(1, #FF1A00));color:transparent;-webkit-background-clip:text;font-size:13px;');
+    },
     computed: {
       ...mapGetters([
         'songList',
