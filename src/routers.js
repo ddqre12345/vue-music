849cf736697d8b08bbc54e9dde3d9473055c96c7
hutingji <hutingji@zhijin.com>
2017-07-18 15:23:18 +0800

    优化模块懒加载配置，去除生产环境map

--- src/routers.js
+++ src/routers.js
@@ -5,16 +5,16 @@ const router = [{
   path: '/find',  //  引导页
   name: 'index',
   component (resolve) {
-    require.ensure(['./views/index'], () => {
-      resolve(require('./views/index'));
+    require.ensure(['@/views/index'], () => {
+      resolve(require('@/views/index'));
     });
   },
   children: [{
     path: '/find',  //  发现
     name: 'find',
     component (resolve) {
-      require.ensure(['./views/find/find'], () => {
-        resolve(require('./views/find/find'));
+      require.ensure(['@/views/find/find'], () => {
+        resolve(require('@/views/find/find'));
       });
     },
     meta: { keepAlive: true }
@@ -23,99 +23,99 @@ const router = [{
 }, {
   path: '/search',  //  搜索页
   name: 'search',
-  component (resolve) {
-    require.ensure(['./views/search/search'], () => {
-      resolve(require('./views/search/search'));
+  component: (resolve) => {
+    import('@/views/search/search').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: true }
 }, {
   path: '/player/:id', //  单曲播放页
   name: 'player',
-  component (resolve) {
-    require.ensure(['./views/detail/player/player'], () => {
-      resolve(require('./views/detail/player/player'));
+  component: (resolve) => {
+    import('@/views/detail/player/player').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/playLists/:id',  //  歌单详情页
   name: 'playLists',
-  component (resolve) {
-    require.ensure(['./views/detail/playList/playlists'], () => {
-      resolve(require('./views/detail/playList/playlists'));
+  component: (resolve) => {
+    import('@/views/detail/playList/playlists').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/singer/:id',  //  歌手详情页
   name: 'singer',
-  component (resolve) {
-    require.ensure(['./views/detail/singer/singer'], () => {
-      resolve(require('./views/detail/singer/singer'));
+  component: (resolve) => {
+    import('@/views/detail/singer/singer').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/album/:id',  // 专辑详情页
   name: 'album',
-  component (resolve) {
-    require.ensure(['./views/detail/album/album'], () => {
-      resolve(require('./views/detail/album/album'));
+  component: (resolve) => {
+    import('@/views/detail/album/album').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/user/:id',  // 用户详情页
   name: 'user',
-  component (resolve) {
-    require.ensure(['./views/detail/user/user'], () => {
-      resolve(require('./views/detail/user/user'));
+  component: (resolve) => {
+    import('@/views/detail/user/user').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/ranking/:idx',  // 榜单详情页
   name: 'ranking',
-  component (resolve) {
-    require.ensure(['./views/detail/ranking/ranking'], () => {
-      resolve(require('./views/detail/ranking/ranking'));
+  component: (resolve) => {
+    import('@/views/detail/ranking/ranking').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/mv/:id',  // 视频播放
   name: 'mv',
-  component (resolve) {
-    require.ensure(['./views/detail/mvPlay/mvPlay'], () => {
-      resolve(require('./views/detail/mvPlay/mvPlay'));
+  component: (resolve) => {
+    import('@/views/detail/mvPlay/mvPlay').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/playListComment/:id',  // 歌单评论
   name: 'playListComment',
-  component (resolve) {
-    require.ensure(['./views/detail/playList/playListComment'], () => {
-      resolve(require('./views/detail/playList/playListComment'));
+  component: (resolve) => {
+    import('@/views/detail/playList/playListComment').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/albumComment/:id',  // 专辑评论
   name: 'albumComment',
-  component (resolve) {
-    require.ensure(['./views/detail/album/albumComment'], () => {
-      resolve(require('./views/detail/album/albumComment'));
+  component: (resolve) => {
+    import('@/views/detail/album/albumComment').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
 }, {
   path: '/rankingComment/:id',  // 排行榜歌单评论
   name: 'rankingComment',
-  component (resolve) {
-    require.ensure(['./views/detail/ranking/rankingComment'], () => {
-      resolve(require('./views/detail/ranking/rankingComment'));
+  component: (resolve) => {
+    import('@/views/detail/ranking/rankingComment').then((module) => {
+      resolve(module);
     });
   },
   meta: { keepAlive: false }
